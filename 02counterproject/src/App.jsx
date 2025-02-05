cc1eb614e000c204a9a106d1a16f2504d4752e20
anikettikkal <tikkalaniket2019@gmail.com>
2025-02-05 20:53:19 +0530

    hooks counter project done

--- 02counterproject/src/App.jsx
+++ 02counterproject/src/App.jsx
@@ -2,9 +2,33 @@ import { useState } from 'react'
 
 function App() {
 
+  let [counter, setCounter] = useState(0)
+  let addValue = () => {
+    
+    setCounter(counter+1)
+    if(counter==20){
+      setCounter(20)
+      console.log("value not increases");
+      
+    }
+  }
+
+  let removeValue = () => {
+    setCounter(counter-1)
+    if(counter <= 0){
+      setCounter(0)
+      console.log('not decreasing below 0');
+    }
+  }
+
   return (
     <>
       <h1>aniket</h1>
+      <h2>Counter Project by using Hooks</h2>
+      <h2>counter value : {counter}</h2>
+      <button onClick={addValue}>add value</button> 
+      <br />
+      <button onClick={removeValue}>remove value</button>
     </>
   )
 }
