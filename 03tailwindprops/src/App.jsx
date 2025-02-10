70736d8538b34c51e77738b7b999ffd77a3477a4
anikettikkal <tikkalaniket2019@gmail.com>
2025-02-10 20:56:27 +0530

    props started

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -3,26 +3,17 @@ import React from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Components/Card';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
-        <div>
-          <img class="size-48 shadow-xl rounded-md" alt="" src="https://images.pexels.com/photos/30560164/pexels-photo-30560164/free-photo-of-dazzling-fireworks-display-in-vietnam-night-sky.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" />
-        </div>
-        <div class="flex items-center md:items-start">
-          <span class="text-2xl font-medium">Class Warfare</span>
-          <span class="font-medium text-sky-500">The Anti-Patterns</span>
-          <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-            <span>No. 4</span>
-            <span>·</span>
-            <span>2025</span>
-          </span>
-        </div>
-      </div>
+      <h1 className='bg-green-400 rounded-xl p-5 text-white'>Tailwind Test</h1>
+      <Card username='Aniket' />
+      <Card username="Sharayu"/>
+
     </>
   )
 }
---
+++ 03tailwindprops/src/Components/Card.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+
+function Card({username}) {
+    console.log(username);
+    
+    return (
+        <>
+            <div className="max-w-xs p-6 m-4 rounded-xl shadow-md bg-black">
+                <img
+                    src="https://m.media-amazon.com/images/I/51jeuNZnSIL.jpg"
+                    alt=""
+                    className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
+                />
+                <div className="mt-6 mb-2">
+                    <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
+                        Title
+                    </span>
+                    <h2 className="text-xl font-semibold tracking-wide">{username}</h2>
+                </div>
+                <p className="text-gray-300">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio tempora ipsum soluta
+                    amet
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Card
