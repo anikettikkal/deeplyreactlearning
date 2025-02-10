d97369410659eaa910905019596e3929bb4a420a
anikettikkal <tikkalaniket2019@gmail.com>
2025-02-10 20:12:55 +0530

    tailwind css complete

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -9,7 +9,20 @@ function App() {
 
   return (
     <>
-      <h1>Tailwind Test</h1>
+      <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+        <div>
+          <img class="size-48 shadow-xl rounded-md" alt="" src="https://images.pexels.com/photos/30560164/pexels-photo-30560164/free-photo-of-dazzling-fireworks-display-in-vietnam-night-sky.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" />
+        </div>
+        <div class="flex items-center md:items-start">
+          <span class="text-2xl font-medium">Class Warfare</span>
+          <span class="font-medium text-sky-500">The Anti-Patterns</span>
+          <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+            <span>No. 4</span>
+            <span>·</span>
+            <span>2025</span>
+          </span>
+        </div>
+      </div>
     </>
   )
 }
