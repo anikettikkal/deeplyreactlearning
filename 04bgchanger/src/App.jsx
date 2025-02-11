8d72c613c0f23672a0b0b934e504294adde95e96
anikettikkal <tikkalaniket2019@gmail.com>
2025-02-11 21:41:53 +0530

    all buttons are is in working condition

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -11,70 +11,70 @@ function App() {
     <>
       <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'
-        ><div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-full  '>
+        ><div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-full border-2 '>
             <button onClick={() => { setColor("red") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
               style={{ backgroundColor: 'red', fontSize: "20px" }}>Red
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("orange") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Orange
+              style={{ backgroundColor: 'orange', fontSize: "20px" }}>Orange
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("yellow") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Yellow
+              style={{ backgroundColor: 'yellow', fontSize: "20px" }}>Yellow
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("blue") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
               style={{ backgroundColor: 'red', fontSize: "20px" }}>Blue
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("violet") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Violet
+              style={{ backgroundColor: 'violet', fontSize: "20px" }}>Violet
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("pink") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Pink
+              style={{ backgroundColor: 'pink', fontSize: "20px" }}>Pink
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("green") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Green
+              style={{ backgroundColor: 'green', fontSize: "20px" }}>Green
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("grey") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Grey
+              style={{ backgroundColor: 'grey', fontSize: "20px" }}>Grey
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("chocolate") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Chocolate
+              style={{ backgroundColor: 'chocolate', fontSize: "20px" }}>Chocolate
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("indigo") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Golden
+              style={{ backgroundColor: 'indigo', fontSize: "20px" }}>indigo
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("purple") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>purple
+              style={{ backgroundColor: 'purple', fontSize: "20px" }}>Purple
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("aqua") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Red
+              style={{ backgroundColor: 'aqua', fontSize: "20px" }}>Aqua
             </button>
 
-            <button onClick={() => { setColor("red") }}
+            <button onClick={() => { setColor("olive") }}
               className='outline-none px-4 py-1 rounded-xl font-bold text-white font-stretch-semi-expanded'
-              style={{ backgroundColor: 'red', fontSize: "20px" }}>Red
+              style={{ backgroundColor: 'olive', fontSize: "20px" }}>Olive
             </button>
           </div>
 
