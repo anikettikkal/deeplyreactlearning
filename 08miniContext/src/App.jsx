f0b5154682b46b1f9e58bd0e640851a6cbd384c4
anikettikkal <tikkalaniket2019@gmail.com>
2025-02-21 14:12:52 +0530

    context api

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>Aniket</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
