70ccfa960df9e9b3d9a92b30f2eea5c62e4d85e2
anikettikkal <tikkalaniket2019@gmail.com>
2025-02-26 13:26:16 +0530

    auth services of user login logout created

--- megablog12/src/appwrite/auth.js
+++ megablog12/src/appwrite/auth.js
@@ -12,7 +12,47 @@ export class AuthService {
         this.account = new Account(this.client);
     }
 
-    
+    async createAcccount({email, password,name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(),email, password, name);
+            if(userAccount){
+                // call another method
+                return this.login({email,password});
+
+            } else {
+                return userAccount;
+
+            }
+        } catch (error){
+            throw error;
+        }
+    }
+
+    async login({email,password}){
+        try{
+           return await this.account.createEmailSession
+        } catch (error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        } catch(error){
+            throw error;
+        }
+
+        return null;
+    }
+
+    async logout(){
+        try{
+            return await this.account.deleteSessions('current');
+        } catch(error){
+            throw error;
+        }
+    }
 }
 
 const authService = new AuthService();
