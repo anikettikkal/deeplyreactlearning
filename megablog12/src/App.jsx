ed91b291c764b6be7be8b02f7a365ebae86d4b8e
anikettikkal <tikkalaniket2019@gmail.com>
2025-03-01 15:47:28 +0530

    tailwind import

--- megablog12/src/App.jsx
+++ megablog12/src/App.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
-
+import { useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
+import authService from './appwrite/auth'
+import {login , logout} from './store/authSlice'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+        if (userData) {
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+    })
+    .finally(()=>{
+      setLoading(false)
+    })
+  }, [])
+
+  return !loading ? (
     <>
-      <h1>a blog with appwrite</h1>
+    <div className='min-h-screen'></div>
     </>
-  )
+  ) : null
+  
 }
 
 export default App
