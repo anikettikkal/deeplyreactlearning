8c97166c91a6409b57a612b7a6096c16123f6515
anikettikkal <tikkalaniket2019@gmail.com>
2025-03-06 13:38:12 +0530

    all pages done

--- megablog12/src/pages/AllPost.jsx
+++ megablog12/src/pages/AllPost.jsx
@@ -4,8 +4,26 @@ import { PostCard, Container } from "../Components";
 import appwriteService from "../appwrite/config"
 
 function AllPost(){
+    const [posts, setPosts]= useState([])
+    useEffect(() => {}, [])
+    appwriteService.getPost([]).then((post) =>{
+        if (posts) {
+            setPosts(posts.documents)
+        }
+    })
+
     return(
-        <div></div>
+        <div className="w-full py-8">
+            <Container>
+                <div className="flex flex-wrap">
+                {posts.map( (post) =>{
+                    <div key={post.$id} className="p-2 w-1/4">
+                        <PostCard post={post} />
+                    </div>
+                })}
+                </div>
+            </Container>
+        </div>
     )
 }
 export default AllPost
