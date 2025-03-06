8ffe47915c3df082661330156114770569464f27
anikettikkal <tikkalaniket2019@gmail.com>
2025-03-06 12:52:14 +0530

    pages started

---
+++ megablog12/src/pages/AllPost.jsx
@@ -0,0 +1,11 @@
+import React,{useState, useEffect} from "react";
+import appwriteService from '../appwrite/config';
+import { PostCard, Container } from "../Components";
+import appwriteService from "../appwrite/config"
+
+function AllPost(){
+    return(
+        <div></div>
+    )
+}
+export default AllPost
