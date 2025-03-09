b945ebf6b58ee1ee15e72717299fe49f70c420f8
anikettikkal <tikkalaniket2019@gmail.com>
2025-03-10 01:48:34 +0530

    now i know the concept of when the react app will re-rendered

--- interview-discussion/src/App.jsx
+++ interview-discussion/src/App.jsx
@@ -4,24 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [value, setValue]= useState(1)
+  console.log("app rendered", Math.random());
+  
+  // const [value, setValue]= useState(1)
+  const [value, setValue]= useState({
+    value:0,
+  })
+
   // const [multiplyValue, setMultiplyValue] = useState(1)
 
-  let multiplyValue= value*5
+  // let multiplyValue= value*5
 
-  const multiplybyfive = ()=>{
-    // setMultiplyValue(value *5)
-    setValue(value+1)
-  }
+  // const multiplybyfive = ()=>{
+  //   // setMultiplyValue(value *5)
+  //   setValue(value+1)
+  // }
   
-
+  const clickMe =()=>{
+    setValue({
+      value:0,
+    })    
+  }
   
 
   return (
     <>
-      <h1>Main Value:{value}</h1>
-      <button onClick={multiplybyfive}> click to multiply by 5</button>
-      <h2>Multiplied Value: {multiplyValue}</h2>
+      <h1>Main Value:{value.value}</h1>
+      <button onClick={clickMe}> click to multiply by 5</button>
+      {/* <h2>Multiplied Value: {multiplyValue}</h2> */}
     </>
   )
 }
